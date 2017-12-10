acf1ac47a7af8b0585bda8e21266cc52c969314e
Paweł Kolański <pakolanski@gmail.com>
2017-12-10 23:07:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+ReceptionPanel
+==============
+
+A Symfony project created on October 15, 2017, 7:07 pm.
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Paweł
+ * Date: 26.10.2017
+ * Time: 00:24
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="`user`")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+}
